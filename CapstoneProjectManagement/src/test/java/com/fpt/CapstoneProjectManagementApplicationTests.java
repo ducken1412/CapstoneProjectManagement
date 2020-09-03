a4a63e59a1e1c9bd853064a1ba2cfac13e5484a7
DESKTOP-KMNS09Q\Admin <ducken1412@gmail.com>
2020-09-03 20:37:09 +0700

    base project

---
+++ CapstoneProjectManagement/src/test/java/com/fpt/CapstoneProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
