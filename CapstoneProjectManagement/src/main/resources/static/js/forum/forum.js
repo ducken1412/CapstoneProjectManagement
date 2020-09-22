c47e7d1b1917a4484e0d49d4a4dc02115d9ae3c0
DUCDD-SE04936 <ducken1412@gmail.com>
2020-09-23 00:34:36 +0700

    Forum template

---
+++ CapstoneProjectManagement/src/main/resources/static/js/forum/forum.js
@@ -0,0 +1,23 @@
+$("#btn-addTopic").click(function() {
+	getFormAddTopic();
+});
+
+function getFormAddTopic() {
+	$.ajax({
+		url : '/add-topic',
+		type : 'GET',
+		success : function(data) {
+			$('#modal-content').html(data);
+		},
+		error: function(xhr){
+			
+			$('#modal-content').html("Error");
+			if(xhr.status == 302 || xhr.status == 200){
+				window.location.href = '/forum'
+			}
+		},
+		complete: function() {
+			
+		}
+	});
+}
