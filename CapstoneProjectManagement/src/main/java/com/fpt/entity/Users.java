556c9159f5c9f55ca8aacf900382aaee974cc1b0
DUCDD <ducken1412@gmail.com>
2020-09-24 22:20:53 +0700

    forum

--- CapstoneProjectManagement/src/main/java/com/fpt/entity/Users.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/entity/Users.java
@@ -73,18 +73,21 @@ public class Users implements Serializable {
 	private ReportDetails reportDetail;
 	@OneToOne(mappedBy = "assessor")
 	private EvaluationDetails evaluationDetail;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "author")
+	private List<Posts> posts;
 
 	public Users() {
 		super();
 	}
 
-	public Users(String userName, String encrytedPassword, String firstName, String lastName, Date birthDate,
+	public Users(String id, String userName, String encrytedPassword, String firstName, String lastName, Date birthDate,
 			Integer gender, String phone, String email, Date createdDate, Locations location, String description,
 			Status status, List<UserRoles> roleUser, List<HistoryRecords> historyRecords,
 			CapstoneProjects capstoneProject, Notifications notificationSend, List<Notifications> notificationReceives,
 			Reports reportSend, Reports reportReceive, List<Reports> reportReceives, List<Comments> comments,
-			ReportDetails reportDetail, EvaluationDetails evaluationDetail) {
+			ReportDetails reportDetail, EvaluationDetails evaluationDetail, List<Posts> posts) {
 		super();
+		this.id = id;
 		this.userName = userName;
 		this.encrytedPassword = encrytedPassword;
 		this.firstName = firstName;
@@ -108,6 +111,7 @@ public class Users implements Serializable {
 		this.comments = comments;
 		this.reportDetail = reportDetail;
 		this.evaluationDetail = evaluationDetail;
+		this.posts = posts;
 	}
 
 	public String getId() {
@@ -302,8 +306,13 @@ public class Users implements Serializable {
 		this.evaluationDetail = evaluationDetail;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public List<Posts> getPosts() {
+		return posts;
 	}
 
+	public void setPosts(List<Posts> posts) {
+		this.posts = posts;
+	}
+
+	
 }
