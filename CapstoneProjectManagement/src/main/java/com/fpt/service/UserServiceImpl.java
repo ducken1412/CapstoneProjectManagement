c902e028b20bf0ec9050bdffc0a4417c47c8faf8
khanhnn1611 <khanhnnse05045@fpt.edu.vn>
2020-09-27 19:59:27 +0700

    Revert "update dropdown search"
    This reverts commit 4818fff3cb492939cc13c1d12827d55929c570d5.

--- CapstoneProjectManagement/src/main/java/com/fpt/service/UserServiceImpl.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/service/UserServiceImpl.java
@@ -1,46 +1,19 @@
 package com.fpt.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.fpt.entity.Users;
 import com.fpt.repository.UserRepository;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
 	public Users findById(String id) {
+		// TODO Auto-generated method stub
 		return userRepository.findById(id).orElse(null);
-	}
-
-	@Override
-	public boolean deleteUser(String id) {
-		try {
-			userRepository.deleteById(id);
-		} catch (IllegalArgumentException e) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean save(Users user) {
-		try {
-			userRepository.save(user);
-		} catch (IllegalArgumentException e) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public List<Users> findAll() {
-		return userRepository.findAll();
 	}
 
 }
