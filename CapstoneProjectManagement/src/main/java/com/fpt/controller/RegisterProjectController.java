06f1dec2f3200705e97720944c717c6073201916
khanhnn1611 <khanhnnse05045@fpt.edu.vn>
2020-09-27 19:58:06 +0700

    update dropdown search

--- CapstoneProjectManagement/src/main/java/com/fpt/controller/RegisterProjectController.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/controller/RegisterProjectController.java
@@ -1,6 +1,6 @@
 package com.fpt.controller;
 
-import javax.validation.Valid;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,33 +13,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fpt.dto.RegisterProjectDTO;
 import com.fpt.entity.CapstoneProjects;
+import com.fpt.entity.Users;
 import com.fpt.service.RegisterProjectService;
 import com.fpt.service.StatusService;
+import com.fpt.service.UserService;
 
 @Controller
-
 public class RegisterProjectController {
-	@GetMapping("/registerproject")
-	public String registerProject() {
-		return "home/register-project";
-	}
-	
+//	@GetMapping("/registerproject")
+//	public String registerProject() {
+//		return "home/register-project";
+//	}
+
 	@Autowired
 	private RegisterProjectService projectService;
-	
+
 	@Autowired
 	private StatusService statusService;
-	
-	
-	//add a project in the database
-	@ResponseBody
-	@RequestMapping(value= "/register", method= RequestMethod.POST)
-	public String addRegisterPoject(RegisterProjectDTO dto ,BindingResult result, Model model) {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/registerproject")
+	public String getRegisterForm(Model model) {
+		List<Users> users = userService.findAll();
+		System.out.println(users);
+		model.addAttribute("users", users);
+		return "home/register-project";
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String addRegisterPoject(RegisterProjectDTO dto, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			return "register-project";
 		}
 		CapstoneProjects projects = new CapstoneProjects();
-		//register status = 1 (seding)
+		// register status = 1 (seding)
 		int status_id = 1;
 		projects.setName(dto.getName());
 		projects.setDescription(dto.getDescription());
@@ -48,7 +57,7 @@ public class RegisterProjectController {
 		projects.setSpecialty(dto.getSpecialty());
 		projects.setProgram(dto.getProgram());
 		projects.setStatus(statusService.getStatusById(status_id));
-		//System.out.println(capstoneProjects);
+		// System.out.println(capstoneProjects);
 		projectService.saveRegisterProject(projects);
 		return "redirect:list";
 	}
--- CapstoneProjectManagement/src/main/java/com/fpt/service/UserService.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/service/UserService.java
@@ -1,8 +1,17 @@
 package com.fpt.service;
 
+import java.util.List;
+
 import com.fpt.entity.Users;
 
 public interface UserService {
+
 	Users findById(String id);
+
+	boolean deleteUser(String id);
+
+	boolean save(Users user);
+
+	List<Users> findAll();
 
 }
