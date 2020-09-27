b76e7c96d9c1ab2eec568c07777079991c02e0c2
LongHazard <longndse05906@fpt.edu.vn>
2020-09-27 20:04:39 +0700

    Long nộp code danh sách giảng viên

--- CapstoneProjectManagement/src/main/java/com/fpt/controller/ListLecturersController.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/controller/ListLecturersController.java
@@ -1,7 +1,21 @@
 package com.fpt.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fpt.controller.ListLecturersController;
+import com.fpt.dto.ListLecturersDTO;
+import com.fpt.service.ListLecturersService;
+import com.fpt.dto.ListLecturersDTO;
+import com.fpt.service.ListLecturersService;
 @Controller
 
 public class ListLecturersController {
@@ -9,5 +23,20 @@ public class ListLecturersController {
 	public String registerProject() {
 		return "home/listlecturers";
 	}
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ListLecturersController.class);
+	
+	@Autowired
+	private ListLecturersService lecturersServiceImpl;
+	
+	@RequestMapping(value = "/templates/home/listlecturers", method = RequestMethod.GET)
+	public String getListUser(Model model) {
+		LOGGER.info("Running on getListLecturers method of UserController");
+		
+		List<ListLecturersDTO> lecturer = lecturersServiceImpl.getAllLecturersDTOActive();
+		model.addAttribute("lecturers", lecturer);
+		return "home/listlecturers";
+	}
+
 
 }
---
+++ CapstoneProjectManagement/src/main/java/com/fpt/repository/ListLecturers.java
@@ -0,0 +1,17 @@
+package com.fpt.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.fpt.dto.ListLecturersDTO;
+import com.fpt.entity.Users;
+@Repository
+public interface ListLecturers extends JpaRepository<Users, String>{
+	
+	@Query("select new com.fa.dto.UserDTO(u) From Users u  where status = ?1")
+	public List<ListLecturersDTO> findAllLecturersDTOByStatus(Integer status);
+
+}
