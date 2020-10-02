2a4e15b2a93916eaf8398a3eebc4881403245f38
nh0khoang <khangnmse05904@fpt.edu.vn>
2020-10-02 23:05:24 +0700

    roleuser

---
+++ CapstoneProjectManagement/src/main/java/com/fpt/service/UserRoleService.java
@@ -0,0 +1,20 @@
+package com.fpt.service;
+
+import java.util.List;
+
+import com.fpt.entity.UserRoleKey;
+import com.fpt.entity.UserRoles;
+
+
+
+public interface UserRoleService {
+
+	List<String> getRoleNamesByUserId(String userId);
+
+	boolean saveRoleUser(UserRoles roleUser);
+
+	boolean removeAllRoleOfUserByUserId(Integer userId);
+
+	UserRoles isExists(UserRoleKey roleUserKey);
+
+}
---
+++ CapstoneProjectManagement/src/main/java/com/fpt/service/UserRoleServiceImpl.java
@@ -0,0 +1,56 @@
+package com.fpt.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fpt.entity.UserRoleKey;
+import com.fpt.entity.UserRoles;
+import com.fpt.repository.UserRolesRepository;
+
+
+
+@Service
+public class UserRoleServiceImpl implements UserRoleService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserRoleServiceImpl.class);
+	
+	@Autowired
+	private UserRolesRepository userRolesRepository;
+
+	
+
+	@Override
+	public boolean saveRoleUser(UserRoles userRole) {
+		try {
+			userRolesRepository.save(userRole);
+			return true;
+		}catch (Exception e) {
+			LOGGER.error("Save RoleUser fail :", e);
+			return false;
+		}
+	}
+	
+	@Override
+	public boolean removeAllRoleOfUserByUserId(Integer userId) {
+		try {
+			userRolesRepository.removeByUserId(userId);
+			return true;
+		}catch (Exception e) {
+			LOGGER.error("Remove all role of user " + userId.intValue() + " fail : ", e);
+			return false;
+		}
+	}
+	
+	@Override
+	public UserRoles isExists(UserRoleKey roleUserKey) {
+		return userRolesRepository.findById(roleUserKey).orElse(null);
+	}
+
+	@Override
+	public List<String> getRoleNamesByUserId(String userId) {
+		return userRolesRepository.getRoleNamesByUserId(userId);
+	}
+}
