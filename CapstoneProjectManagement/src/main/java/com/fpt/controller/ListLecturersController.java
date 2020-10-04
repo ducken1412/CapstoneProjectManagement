a7ddc3a7be35b6a0bcaf463bda4c7c661ffe2678
LongHazard <longndse05906@fpt.edu.vn>
2020-10-04 13:19:49 +0700

    long sua controller

--- CapstoneProjectManagement/src/main/java/com/fpt/controller/ListLecturersController.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/controller/ListLecturersController.java
@@ -39,8 +39,6 @@ public class ListLecturersController {
 		
 		List<Users> lecturer = lecturersServiceImpl.getAllLecturersDTOActive();
 		model.addAttribute("lecturer", lecturer);
-		
-		
 		return "home/listlecturers";
 	}
 	
