da7b1d6786f0169409052e357225c80ca1b3d3ef
nh0khoang <khangnmse05904@fpt.edu.vn>
2020-10-08 15:01:35 +0700

    detail project

--- CapstoneProjectManagement/src/main/java/com/fpt/repository/CapstoneProjectDetailRepository.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/repository/CapstoneProjectDetailRepository.java
@@ -1,9 +1,17 @@
 package com.fpt.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.fpt.entity.CapstoneProjectDetails;
+import com.fpt.entity.CapstoneProjects;
+import com.fpt.entity.Users;
 
-public interface CapstoneProjectDetailRepository extends JpaRepository<CapstoneProjectDetails, Integer>{
+public interface CapstoneProjectDetailRepository extends JpaRepository<CapstoneProjectDetails, CapstoneProjects>{
+	@Query("SELECT ru.CapstoneProjectDetails.user FROM CapstoneProjects ru WHERE ru.capstoneProjects.id = ?1")
+	List<String> getUserByCapstoneProjectDetailId(Integer id);
+	
 	
 }
--- CapstoneProjectManagement/src/main/java/com/fpt/service/CapstoneProjectDetailService.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/service/CapstoneProjectDetailService.java
@@ -1,11 +1,15 @@
 package com.fpt.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.fpt.entity.CapstoneProjectDetails;
+import com.fpt.entity.Users;
 
 @Service
 public interface CapstoneProjectDetailService {
 	//add the capstone project detail
 	boolean addCapstonprojectDetail(CapstoneProjectDetails capstoneProjectDetails);
+	List<String> getUserByCapstoneProjectDetailId(Integer id);
 }
