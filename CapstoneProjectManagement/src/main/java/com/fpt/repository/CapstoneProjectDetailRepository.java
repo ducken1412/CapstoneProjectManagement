85e172388077a1ae7a63fb6b21d0c50c32c18557
DUCDD-SE04936 <ducken1412@gmail.com>
2020-10-11 02:40:36 +0700

    update forum

--- CapstoneProjectManagement/src/main/java/com/fpt/repository/CapstoneProjectDetailRepository.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/repository/CapstoneProjectDetailRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 import com.fpt.entity.CapstoneProjectDetails;
 import com.fpt.entity.CapstoneProjects;
 import com.fpt.entity.Users;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CapstoneProjectDetailRepository extends JpaRepository<CapstoneProjectDetails, CapstoneProjects>{
 	//query loi
-	@Query("SELECT ru.user FROM CapstoneProjectDetails ru WHERE ru.capstoneProjects = ?1")
-	List<CapstoneProjectDetails> getUserByCapstoneProjectDetailId(Integer id);
+//	@Query("SELECT ru.user FROM CapstoneProjectDetails ru WHERE ru.capstoneProjects = ?1")
+//	List<CapstoneProjectDetails> getUserByCapstoneProjectDetailId(Integer id);
 	
 	
 }
