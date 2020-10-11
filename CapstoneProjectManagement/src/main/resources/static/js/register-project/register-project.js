ec8d2d5dc871e734572d9b816b0e5523f2fe7d27
KienBT-SE05182 <kienmaxhp@gmail.com>
2020-10-11 18:44:01 +0700

    fix bug add member

--- CapstoneProjectManagement/src/main/resources/static/js/register-project/register-project.js
+++ CapstoneProjectManagement/src/main/resources/static/js/register-project/register-project.js
@@ -1,6 +1,38 @@
-// Material Select Initialization
-$(document).ready(function() {
-	//alert("asd")
-$('.mdb-select').materialSelect();
-});
+$(document).on("submit", "#register", function (e) {
+    e.preventDefault();
+    let dataForm = $("#register").serialize();
+    $.ajax({
+        url: "/register",
+        type: "POST",
+        data: dataForm,
+        success: function (data) {
+            $("#form-content").html(data)
+            console.log(data)
+            let check = false
+            $(".error-mess").each(function loop() {
+                if($(this).text() !== ""){
+                    check = true
+                }
+            });
+            if(!check) {
+                $.ajax({
+                    url: "/get-member-form",
+                    type: "GET",
+                    data: dataForm,
+                    success: function (data1) {
+                        $("#modal-content").html(data1);
+                        $('#member-container').modal('show');
 
+                    },
+                    error: function (xhr) {
+                        $("#modal-content").html("Error");
+                    },
+                });
+            }
+        },
+        error: function (xhr) {
+            console.log('as');
+            $("#modal-content").html("Error");
+        },
+    });
+});
