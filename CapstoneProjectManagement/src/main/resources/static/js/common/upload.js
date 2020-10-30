bf3b8937166e22346c7e715922c9464f1b29010f
KienBT-SE05182 <kienmaxhp@gmail.com>
2020-10-31 01:26:30 +0700

    fix bug upload

--- CapstoneProjectManagement/src/main/resources/static/js/common/upload.js
+++ CapstoneProjectManagement/src/main/resources/static/js/common/upload.js
@@ -8,10 +8,10 @@ function getFileSizeAndName(input)
     for(var i =0; i<input.files.length; i++)
     {
         var filesizeInBytes = input.files[i].size;
-        var filesizeInMB = (filesizeInBytes / (1024*1024)).toFixed(2);
+        var filesizeInMB = (filesizeInBytes / (1024*1024)).toFixed(100);
         var filename = input.files[i].name;
         //alert("File name is : "+filename+" || size : "+filesizeInMB+" MB || size : "+filesizeInBytes+" Bytes");
-            select.append($('<label id=file'+i+'>'+filename+'</label><br>'));
+        select.append($('<label id=file'+i+'>'+filename+'</label><br>'));
 
     }
 }
