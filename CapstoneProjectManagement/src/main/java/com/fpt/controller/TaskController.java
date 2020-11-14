92cd48263c58f9c4392fcd541d3f9ae4a0257df3
DUCDD <ducken1412@gmail.com>
2020-11-15 01:31:24 +0700

    no message

--- CapstoneProjectManagement/src/main/java/com/fpt/controller/TaskController.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/controller/TaskController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-    @GetMapping("/db/dashboard")
-    public String getDashboard(){
-        return "admin/dashboard-project";
+public class TaskController {
+    @GetMapping("/db/statistic/{id}")
+    public String getStatistic() {
+        return "admin/statistics-project";
     }
 }
