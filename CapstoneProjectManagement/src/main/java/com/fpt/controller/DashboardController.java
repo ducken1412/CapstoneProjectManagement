2e44a4aa4d5a4f4ec4038be48c77adc52aa70fd9
KienBT-SE05182 <kienmaxhp@gmail.com>
2020-12-06 16:03:08 +0700

    fix header and db

--- CapstoneProjectManagement/src/main/java/com/fpt/controller/DashboardController.java
+++ CapstoneProjectManagement/src/main/java/com/fpt/controller/DashboardController.java
@@ -42,74 +42,79 @@ public class DashboardController {
             return "redirect:/login";
         }
 
-        //get sites and semesters
-        List<Sites> sites = sitesService.findAll();
-        model.addAttribute("sites", sites);
+        try {
+            //get sites and semesters
+            List<Sites> sites = sitesService.findAll();
+            model.addAttribute("sites", sites);
 
-        List<Semesters> semesters = semestersService.findAll();
-        model.addAttribute("semesters", semesters);
+            List<Semesters> semesters = semestersService.findAll();
+            model.addAttribute("semesters", semesters);
 
-        //get max week of statics
-        Integer week =  taskDetailsService.findMaxWeek();
-        Users users = userService.findByEmail(principal.getName());
-        List<Statistics>  statisticsList ;
-        boolean roleHead =false;
-        for (UserRoles role : users.getRoleUser()){
-            if(role.getUserRoleKey().getRole().getName().equalsIgnoreCase(Constant.ROLE_HEAD_DB)){
-                roleHead =true;
-                break;
+            //get max week of statics
+            Integer week =  taskDetailsService.findMaxWeek();
+            Users users = userService.findByEmail(principal.getName());
+            List<Statistics>  statisticsList ;
+            boolean roleHead =false;
+            for (UserRoles role : users.getRoleUser()){
+                if(role.getUserRoleKey().getRole().getName().equalsIgnoreCase(Constant.ROLE_HEAD_DB)){
+                    roleHead =true;
+                    break;
+                }
             }
-        }
-        if (roleHead){
-            statisticsList = statisticsService.getStatisticsWithWeek(week);
-        } else{
-            statisticsList = statisticsService.getStatisticsWithWeekByLecture(week, users.getEmail());
-        }
-
-        StatisticsTotalDTO  statisticsTotalDTO = new StatisticsTotalDTO();
-        int countOnSchedule = 0;
-        int countWarning= 0;
-        int countSerious= 0;
-
-        for (Statistics statistics : statisticsList){
-            if (statistics.getTimeTrackingCurrent() >= 80){
-                countOnSchedule = countOnSchedule + 1;
-            }  else if (statistics.getTimeTrackingCurrent() >= 50 && statistics.getTimeTrackingCurrent() < 80){
-                countWarning = countWarning + 1 ;
-            }  else {
-                countSerious = countSerious + 1;
+            if (roleHead){
+                statisticsList = statisticsService.getStatisticsWithWeek(week);
+            } else{
+                statisticsList = statisticsService.getStatisticsWithWeekByLecture(week, users.getEmail());
             }
-            
+
+            StatisticsTotalDTO  statisticsTotalDTO = new StatisticsTotalDTO();
+            int countOnSchedule = 0;
+            int countWarning= 0;
+            int countSerious= 0;
+
+            for (Statistics statistics : statisticsList){
+                if (statistics.getTimeTrackingCurrent() >= 80){
+                    countOnSchedule = countOnSchedule + 1;
+                }  else if (statistics.getTimeTrackingCurrent() >= 50 && statistics.getTimeTrackingCurrent() < 80){
+                    countWarning = countWarning + 1 ;
+                }  else {
+                    countSerious = countSerious + 1;
+                }
+
+            }
+
+            if (countOnSchedule != 0 && statisticsList.stream().count() != 0){
+                Double perOnSchedule = Double.valueOf(countOnSchedule) / Double.valueOf(statisticsList.stream().count()) * 100;
+                statisticsTotalDTO.setTotalOnSchedule(perOnSchedule);
+
+            }else {
+                statisticsTotalDTO.setTotalOnSchedule(0.0);
+            }
+
+            if (countWarning != 0 && statisticsList.stream().count() != 0){
+                Double perTotalWarning = Double.valueOf(countWarning) / Double.valueOf(statisticsList.stream().count()) * 100 ;
+                statisticsTotalDTO.setTotalWarning(perTotalWarning);
+
+            }else {
+                statisticsTotalDTO.setTotalWarning(0.0);
+            }
+            if (countSerious != 0 && statisticsList.stream().count() != 0){
+                Double perTotalSerious = Double.valueOf(countSerious) / Double.valueOf(statisticsList.stream().count()) * 100;
+                statisticsTotalDTO.setTotalSerious(perTotalSerious);
+            }else {
+                statisticsTotalDTO.setTotalSerious(0.0);
+            }
+            statisticsTotalDTO.setCountOnSchedule(countOnSchedule);
+            statisticsTotalDTO.setCountSerious(countSerious);
+            statisticsTotalDTO.setCountWarning(countWarning);
+            statisticsTotalDTO.setNumProject(String.valueOf(statisticsList.stream().count()));
+
+            model.addAttribute("StatisticsTotalDTO", statisticsTotalDTO);
+
+
+        }catch (Exception e){
+
         }
-        
-        if (countOnSchedule != 0 && statisticsList.stream().count() != 0){
-            Double perOnSchedule = Double.valueOf(countOnSchedule) / Double.valueOf(statisticsList.stream().count()) * 100;
-            statisticsTotalDTO.setTotalOnSchedule(perOnSchedule);
-
-        }else {
-            statisticsTotalDTO.setTotalOnSchedule(0.0);
-        }
-
-        if (countWarning != 0 && statisticsList.stream().count() != 0){
-            Double perTotalWarning = Double.valueOf(countWarning) / Double.valueOf(statisticsList.stream().count()) * 100 ;
-            statisticsTotalDTO.setTotalWarning(perTotalWarning);
-
-        }else {
-            statisticsTotalDTO.setTotalWarning(0.0);
-        }
-        if (countSerious != 0 && statisticsList.stream().count() != 0){
-            Double perTotalSerious = Double.valueOf(countSerious) / Double.valueOf(statisticsList.stream().count()) * 100;
-            statisticsTotalDTO.setTotalSerious(perTotalSerious);
-        }else {
-            statisticsTotalDTO.setTotalSerious(0.0);
-        }
-        statisticsTotalDTO.setCountOnSchedule(countOnSchedule);
-        statisticsTotalDTO.setCountSerious(countSerious);
-        statisticsTotalDTO.setCountWarning(countWarning);
-        statisticsTotalDTO.setNumProject(String.valueOf(statisticsList.stream().count()));
-
-        model.addAttribute("StatisticsTotalDTO", statisticsTotalDTO);
-
         return "admin/dashboard-project";
     }
 
