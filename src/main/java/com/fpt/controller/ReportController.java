cb0cb167bb96c9bcdceb14e305013380a1d07e0c
DESKTOP-KMNS09Q\Admin <ducken1412@gmail.com>
2020-12-06 14:55:44 +0700

    fix report

--- src/main/java/com/fpt/controller/ReportController.java
+++ src/main/java/com/fpt/controller/ReportController.java
@@ -107,10 +107,16 @@ public class ReportController {
                 }
             }
             if (checkReport) {
+                if(user.getRoleUser().get(0).getUserRoleKey().getRole().getId() == 1){
+                    model.addAttribute("checkReportLeader", true);
+                }
                 model.addAttribute("checkReport", true);
                 Integer reportId = reportService.getReportByUserIdMax(user.getId());
                 model.addAttribute("reportId", reportId);
             } else {
+                model.addAttribute("checkReport", false);
+            }
+            if(user.getRoleUser().get(0).getUserRoleKey().getRole().getId() == 2){
                 model.addAttribute("checkReport", false);
             }
         } catch (Exception e) {
@@ -151,7 +157,9 @@ public class ReportController {
         } catch (Exception ex) {
 
         }
-
+        if(user.getRoleUser().get(0).getUserRoleKey().getRole().getId() == 1){
+            model.addAttribute("checkReportLeader", true);
+        }
         return "home/report-detail";
     }
 
